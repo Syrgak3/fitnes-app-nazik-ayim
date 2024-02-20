b9351ba4134090fe03a30ebe20a77732f32ec6a6
Syrgak <Kary6ekov.s@gmail.com>
2024-02-20 16:46:10 +0100

    project skeleton created

---
+++ src/main/java/com/example/nazikayim/NazikAyimApplication.java
@@ -0,0 +1,13 @@
+package com.example.nazikayim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NazikAyimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NazikAyimApplication.class, args);
+	}
+
+}
